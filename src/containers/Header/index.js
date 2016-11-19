10d23d5705215177c9d02cbdfe0e045219d23575
Spen Taylor <spen_@hotmail.co.uk>
2016-11-19 14:49:57 +1100

    Add Eslint

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -2,7 +2,7 @@
  * External Dependencies
  */
 import React, { Component } from 'react';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
 
 export default class Header extends Component {
     render() {
