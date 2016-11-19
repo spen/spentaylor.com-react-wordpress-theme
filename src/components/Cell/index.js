44e4e1a8fe7c3abac2c67397b0f3d66c97b99abc
Spen Taylor <spen_@hotmail.co.uk>
2016-11-19 20:53:18 +1100

    Add Cell component (#10)

---
+++ src/components/Cell/index.js
@@ -0,0 +1,11 @@
+/**
+ * External Dependencies
+ */
+import styled, { css } from 'styled-components';
+
+const cellStyles = css`
+    display: table-cell;
+    vertical-align: middle;
+`;
+
+export default styled.div`${ cellStyles }`;
