9afa00499ea93868e445b7d3ee477cba308f455b
Spen Taylor <spen_@hotmail.co.uk>
2016-11-21 02:12:15 +1100

    Add custom routing reducer to be able to cache the last content route

--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,39 @@
 /**
  * External Dependencies
  */
-import { routerReducer } from 'react-router-redux';
+import { LOCATION_CHANGE } from 'react-router-redux';
 import { combineReducers } from 'redux';
+
+/**
+ * Internal Dependencies
+ */
+import contentPaths from 'constants/contentPaths';
+
+const routeInitialState = {
+	locationBeforeTransitions: null,
+};
+
+export function routerReducer( state = routeInitialState, action ) {
+	switch ( action.type ) {
+		case LOCATION_CHANGE: {
+			const newState = {
+				...state,
+				locationBeforeTransitions: action.payload,
+			};
+
+			if (
+				action.payload.pathname &&
+				contentPaths.indexOf( action.payload.pathname ) !== -1
+			) {
+				newState.previousContentPath = action.payload.pathname;
+			}
+
+			return newState;
+		}
+		default:
+			return state;
+	}
+}
 
 export default function createReducer( asyncReducers ) {
 	return combineReducers( {
