ce61326e19871e1e04ba5615681797f74b9d2a51
Spen Taylor <spen_@hotmail.co.uk>
2016-11-21 11:54:14 +1100

    Header styles toggle depending on content or intro shown

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -10,11 +10,11 @@ import StyledHeader from './styles/Header';
 
 export default class Header extends Component {
 	render() {
+		const { showContent } = this.props;
+
 		return (
-			<StyledHeader show intro>
-				<ul role="nav">
-					<ShowContentButton />
-				</ul>
+			<StyledHeader show showContent={ showContent }>
+				<ShowContentButton />
 			</StyledHeader>
 		);
 	}
--- src/containers/Header/styles/header.js
+++ src/containers/Header/styles/header.js
@@ -15,7 +15,7 @@ export const headerStyles = css`
     margin-top: 0;
     z-index: 200;
     opacity: ${ props => props.show ? 1 : 0 };
-    background-color: ${ props => props.intro ? 'rgba(0,0,0,0.1)' : 'rgba(255,90,110,1)' };
+    background-color: ${ props => props.showContent ? 'rgba(255,90,110,1)' : 'rgba(0,0,0,0.1)' };
 `;
 
 const Header = styled.div`${ headerStyles }`;
