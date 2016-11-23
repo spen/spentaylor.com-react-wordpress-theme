e6b72b73a6df284d03889de158ee319d2fa43f7a
Spen Taylor <spen_@hotmail.co.uk>
2016-11-24 10:45:36 +1100

    Add Paper Component. Render blog content (#29)

---
+++ src/components/Paper/index.js
@@ -0,0 +1,15 @@
+/**
+ * External Dependencies
+ */
+import styled, { css } from 'styled-components';
+
+const paperStyles = css`
+	margin: 10px 20px;
+	max-width: 960px;
+	background-color: #fff;
+	box-shadow: 0 4px 0 #eee;
+	padding: 2em 4em;
+	position: relative;
+`;
+
+export default styled.div`${ paperStyles }`;
