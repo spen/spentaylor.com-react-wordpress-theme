aa2b9b93272ad4f4397f6a90abf837e759796622
Spen Taylor <spen_@hotmail.co.uk>
2016-11-26 22:57:42 +1100

    Added previous & next post selectors

--- src/containers/Header/NextButton/index.js
+++ src/containers/Header/NextButton/index.js
@@ -2,21 +2,41 @@
  * External Dependencies
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import RightIcon from 'react-icons/lib/fa/angle-right';
 import { Link } from 'react-router';
 
-export default class extends Component {
-	render() {
-		const { className, showContent } = this.props;
+/**
+ * Internal Dependencies
+ */
+import { getNextPostSlug } from 'state/blog/selectors';
 
-		if ( ! showContent ) {
+export class NextButton extends Component {
+	render() {
+		const { className, showContent, targetPostSlug } = this.props;
+
+		if ( ! showContent || ! targetPostSlug ) {
 			return null;
 		}
 
 		return (
-			<Link className={ className } to={ 'nowhere' }>
+			<Link
+				to={ `/blog/${ targetPostSlug }` }
+				className={ className }
+				style={ { right: '0', position: 'absolute' } }
+			>
 				<RightIcon height="100%" />
 			</Link>
 		);
 	}
 }
+
+function mapStateToProps( state ) {
+	const targetPostSlug = getNextPostSlug( state );
+
+	return {
+		targetPostSlug,
+	};
+}
+
+export default connect( mapStateToProps )( NextButton );
