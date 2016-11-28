fdc13c071304a4cb9674207f93575e50dedaf4d8
Spen Taylor <spen_@hotmail.co.uk>
2016-11-28 23:20:55 +1100

    Add ContentArea component to give better scrolling of the content

---
+++ src/components/ContentArea/styles/Scroller.js
@@ -0,0 +1,13 @@
+/**
+ * External Dependencies
+ */
+import styled, { css } from 'styled-components';
+
+const contentAreaStyles = css`
+	overflow-y: scroll;
+    position: relative;
+    height: 100%;
+    width: 100%;
+`;
+
+export default styled.div`${ contentAreaStyles }`;
---
+++ src/components/ContentArea/styles/Wrapper.js
@@ -0,0 +1,14 @@
+/**
+ * External Dependencies
+ */
+import styled, { css } from 'styled-components';
+
+const contentAreaStyles = css`
+    position: absolute;
+    padding-top: 60px;
+    top: 50%;
+    height: 50%;
+    width: 100%;
+`;
+
+export default styled.div`${ contentAreaStyles }`;
