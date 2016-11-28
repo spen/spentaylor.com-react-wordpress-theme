5786259413b3acc791ad02fa71a1d98deaf7e230
Spen Taylor <spen_@hotmail.co.uk>
2016-11-28 16:29:47 +1100

    Add basic blog route transitions

--- src/containers/BlogPage/index.js
+++ src/containers/BlogPage/index.js
@@ -2,14 +2,30 @@
  * External Dependencies
  */
 import React, { Component } from 'react';
+import { RouteTransition } from 'react-router-transition';
+
+const mapStyles = styles => ( {
+	transform: `translateX(${ styles.translateX }%)`,
+	opacity: styles.opacity,
+} );
 
 export class BlogPage extends Component {
-
 	render() {
+		const { location, children } = this.props;
+
 		return (
-			<div>
-				{ this.props.children }
-			</div>
+			<RouteTransition
+				atEnter={ { opacity: 0, translateX: 30 } }
+				atLeave={ { opacity: 0, translateX: -30 } }
+				atActive={ { opacity: 1, translateX: 0 } }
+				className="RouteTransition"
+				component={ false }
+				mapStyles={ mapStyles }
+				pathname={ location.pathname }
+				runOnMount={ false }
+			>
+				{ children }
+			</RouteTransition>
 		);
 	}
 }
