a09337ac9216cc77f852c3fb2fc08ec25cb2b447
Spen Taylor <spen_@hotmail.co.uk>
2016-11-29 14:02:56 +1100

    Add SkeletonPulse animation wrapper, animate group rather than each bar

--- src/components/SkeletonPost/index.js
+++ src/components/SkeletonPost/index.js
@@ -7,21 +7,17 @@ import React from 'react';
  * Internal Dependencies
  */
 import Paper from 'components/Paper';
-import SkeletonBar from 'components/SkeletonBar';
+import SkeletonPulse from 'components/SkeletonPulse';
+import Content from './Content';
 
 export default function() {
 	return (
 		<Paper>
-			<SkeletonBar width="20%" inline />
-			<SkeletonBar width="40%" inline />
-			<SkeletonBar width="40%" />
-			<SkeletonBar width="20%" inline />
-			<SkeletonBar width="30%" inline />
-			<SkeletonBar width="13%" inline />
-			<SkeletonBar width="50%" inline />
-			<SkeletonBar width="20%" inline />
-			<SkeletonBar width="60%" />
-			<SkeletonBar width="40%" />
+			<div style={ { padding: '10px 30px 30px 30px' } } >
+				<SkeletonPulse>
+					<Content />
+				</SkeletonPulse>
+			</div>
 		</Paper>
 	);
 }
