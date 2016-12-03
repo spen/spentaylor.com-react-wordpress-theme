6563cbd1fcdd801bee0e2d4ade2fefe03ffa58dc
Spen Taylor <spen_@hotmail.co.uk>
2016-12-03 14:49:14 +1100

    Maintain content view when going to intro

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -6,23 +6,28 @@ import React, { Component } from 'react';
 /**
  * Internal Dependencies
  */
+import ContentArea from 'components/ContentArea';
 import Header from 'containers/Header';
 import Intro from 'containers/Intro';
-import ContentArea from 'components/ContentArea';
-
 import PageWrap from './styles/App';
+
+let contentComponent;
 
 export default class App extends Component {
 	render() {
 		const { children } = this.props;
 		const showContent = !! children;
 
+		if ( children ) {
+			contentComponent = children;
+		}
+
 		return (
 			<PageWrap open={ showContent }>
 				<Intro />
 				<Header showContent={ showContent } />
 				<ContentArea>
-					{ children }
+					{ contentComponent }
 				</ContentArea>
 			</PageWrap>
 		);
