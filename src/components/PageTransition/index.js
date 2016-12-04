bfa4bc630e3caabca1529b2fad671cdc707b6f12
Spen Taylor <spen_@hotmail.co.uk>
2016-12-04 17:59:56 +1100

    Streamline PageTransition by removng the TransitionSlide Component

--- src/components/PageTransition/index.js
+++ src/components/PageTransition/index.js
@@ -5,11 +5,6 @@ import { get } from 'lodash';
 import React, { Component } from 'react';
 import { RouteTransition } from 'react-router-transition';
 
-/**
- * Internal Dependencies
- */
-import TransitionSlide from './TransitionSlide';
-
 const leftState = { opacity: 0, translateX: 30 };
 const rightState = { opacity: 0, translateX: -30 };
 const activeState = { opacity: 1, translateX: 0 };
@@ -17,6 +12,8 @@ const activeState = { opacity: 1, translateX: 0 };
 const mapStyles = styles => ( {
 	transform: `translateX(${ styles.translateX }%)`,
 	opacity: styles.opacity,
+	position: 'absolute',
+	width: '100%',
 } );
 
 export class PageTransition extends Component {
@@ -29,7 +26,6 @@ export class PageTransition extends Component {
 				atEnter={ direction === 'previous' ? rightState : leftState }
 				atLeave={ direction === 'previous' ? leftState : rightState }
 				atActive={ activeState }
-				component={ TransitionSlide }
 				mapStyles={ mapStyles }
 				pathname={ location.pathname }
 				runOnMount={ false }
