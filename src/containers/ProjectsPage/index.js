4cc09dc125d8f493b0904bc59656264a58e80069
Spen Taylor <spen_@hotmail.co.uk>
2016-12-09 19:25:50 +1100

    Content, Error, Skeleton components set up & rendered for Projects

--- src/containers/ProjectsPage/index.js
+++ src/containers/ProjectsPage/index.js
@@ -1,37 +1,15 @@
 /**
  * External Dependencies
  */
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 
 /**
  * Internal Dependencies
  */
-import ContentContainer from 'components/ContentContainer';
-import { fetchProjects } from 'state/projects/actions';
-import CurrentProject from './CurrentProject';
+import PageTransition from 'components/PageTransition';
 
-export class ProjectsPage extends Component {
-
-	componentWillMount() {
-		this.props.onEnter();
-	}
-
-	render() {
-		return (
-			<ContentContainer>
-				<CurrentProject />
-			</ContentContainer>
-		);
-	}
-}
-
-function mapDispatchToProps( dispatch ) {
-	return {
-		onEnter: () => {
-			dispatch( fetchProjects() );
-		},
-	};
-}
-
-export default connect( null, mapDispatchToProps )( ProjectsPage );
+export default ( { children, location } ) => (
+	<PageTransition location={ location }>
+		{ children }
+	</PageTransition>
+);
