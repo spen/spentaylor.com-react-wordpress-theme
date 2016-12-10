b53853ab171576e82ee30da8b506929e6106079b
Spen Taylor <spen_@hotmail.co.uk>
2016-12-10 19:17:37 +1100

    Add styles to show error states on inputs & textareas

--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -8,9 +8,9 @@ import React from 'react';
  */
 import Input from 'components/Input';
 
-export default ( { input, label, placeholder, type } ) => (
+export default ( { input, label, meta: { touched, error }, placeholder, type } ) => (
 	<div>
 		{ label && <label>{ label }:</label> }
-		<Input { ...input } type={ type } placeholder={ placeholder } />
+		<Input { ...input } type={ type } placeholder={ placeholder } error={ touched && error } />
 	</div>
 );
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,3 +1,6 @@
 export const ERROR_HEX = '#d44';
+export const ERROR_BOX_HEX = '#fcc';
+export const ERROR_BOX_SHADOW_HEX = '#e99';
 
+export const DARK_GREY_HEX = '#444';
 export const LIGHT_GREY_HEX = '#999';
