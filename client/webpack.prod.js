3cb7953a5100a11a36aa0501c6f39ff3ed259d0b
Spen Taylor <spen_@hotmail.co.uk>
2016-12-28 01:17:29 +1100

    Add webpack config files for dev and prod enviroments

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -4,10 +4,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool: 'inline-source-map',
     entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
         './src/index'
     ],
     output: {
@@ -16,41 +13,37 @@ module.exports = {
         publicPath: '/'
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.template.html',
-            inject: true
-        }),
-        new webpack.NoErrorsPlugin(),
-        new ExtractTextPlugin("style.css", {
+        new ExtractTextPlugin('style.css', {
             allChunks: true
-        })
+        }),
     ],
     module: {
         preLoaders: [
-            { test: /\.json$/, loader: 'json'},
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
         ],
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['react-hot-loader/webpack', 'babel'],
+                loaders: ['babel'],
                 exclude: /node_modules/,
                 include: __dirname
             }, {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
             }, {
                 test: /\.png$/,
-                loader: "url-loader?limit=100000"
-            }, {
-                test: /\.jpg$/,
-                loader: "file-loader"
+                loader: 'url-loader?limit=100000'
             }, {
                 test: /\.svg$/,
                 loader: 'babel!svg-react'
             }, {
-                test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
+                test: /\.jpg$/,
+                loader: 'file-loader'
+            }, {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
                 loader: 'file-loader'
             }
         ]
