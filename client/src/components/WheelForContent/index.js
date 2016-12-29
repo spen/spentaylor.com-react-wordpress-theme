39f570d249f1b366f91f64d58df51c823d477d5a
Spen Taylor <spen_@hotmail.co.uk>
2016-12-29 21:02:40 +1100

    Trigger the content route on mouse wheel (trackpad slide)

---
+++ client/src/components/WheelForContent/index.js
@@ -0,0 +1,48 @@
+/**
+ * External Dependencies
+ */
+import { debounce } from 'lodash';
+import React, { PureComponent } from 'react';
+import { browserHistory } from 'react-router';
+import { connect } from 'react-redux';
+
+/**
+ * Internal Dependencies
+ */
+import { getCurrentContentPath } from 'state/routing/selectors';
+
+const wheelForContent = WrappedComponent => {
+	class WheelForContentComponent extends PureComponent {
+
+		constructor(props) {
+			console.log( 'wheelForContent');
+			super(props);
+			this.debouncedNavigate = debounce( this.navigate, 1000, { leading: true } );
+			this.handleWheel = this.handleWheel.bind( this );
+		}
+
+		navigate() {
+			if (this.props.currentContentPath) {
+				browserHistory.push(this.props.currentContentPath);
+			}
+		}
+
+		handleWheel( event ) {
+			if ( event.deltaY > 15 ) {
+				this.debouncedNavigate();
+			}
+		};
+
+		render() {
+			return <WrappedComponent onWheel={ this.handleWheel } />;
+		}
+	}
+
+	return connect(
+		state => ( {
+			currentContentPath: getCurrentContentPath( state ),
+		} )
+	)( WheelForContentComponent );
+}
+
+export default wheelForContent;
