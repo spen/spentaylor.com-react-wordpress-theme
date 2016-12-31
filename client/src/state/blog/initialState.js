fa92f0d078dcb2bc33199a9bce1be9f40268ae00
Spen Taylor <spen_@hotmail.co.uk>
2016-12-31 17:32:51 +1100

    Include direct post in initialData for posts and projects

--- client/src/state/blog/initialState.js
+++ client/src/state/blog/initialState.js
@@ -1,8 +1,13 @@
+const POST_DATA = window.INITIAL_DATA && window.INITIAL_DATA.POST_DATA;
 const initialState = {
 	list: [],
 	total: 0,
 	activePostSlug: null,
 	postsError: {},
 };
+
+if ( POST_DATA && POST_DATA.type === 'post' ) {
+	initialState.list.push( POST_DATA )
+}
 
 export default initialState;
--- client/src/state/projects/initialState.js
+++ client/src/state/projects/initialState.js
@@ -1,8 +1,13 @@
+const POST_DATA = window.INITIAL_DATA && window.INITIAL_DATA.POST_DATA;
 const initialState = {
 	list: [],
 	total: 0,
 	activeSlug: null,
 	error: {},
 };
+
+if ( POST_DATA && POST_DATA.type === 'jetpack-portfolio' ) {
+	initialState.list.push( POST_DATA )
+}
 
 export default initialState;
