4e6afe312bd2b0112ade878c3ad2d35912a82d69
Spen Taylor <spen_@hotmail.co.uk>
2017-01-01 21:49:32 +1100

    Update PageTransition to SLC

--- client/src/components/PageTransition/index.js
+++ client/src/components/PageTransition/index.js
@@ -2,7 +2,7 @@
  * External Dependencies
  */
 import { get } from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import { RouteTransition } from 'react-router-transition';
 
 const leftState = { opacity: 0, translateX: 30 };
@@ -16,28 +16,25 @@ const mapStyles = styles => ( {
 	width: '100%',
 } );
 
-export class PageTransition extends Component {
-	render() {
-		const { location, children } = this.props;
-		const direction = get( location, 'state.direction' );
+const PageTransition = ( { children, location } ) => {
+	const direction = get( location, 'state.direction' );
 
-		if ( ! direction ) {
-			return <div>{ children }</div>;
-		}
-
-		return (
-			<RouteTransition
-				atEnter={ direction === 'previous' ? rightState : leftState }
-				atLeave={ direction === 'previous' ? leftState : rightState }
-				atActive={ activeState }
-				mapStyles={ mapStyles }
-				pathname={ location.pathname }
-				runOnMount={ false }
-			>
-				{ children }
-			</RouteTransition>
-		);
+	if ( ! direction ) {
+		return <div>{ children }</div>;
 	}
-}
+
+	return (
+		<RouteTransition
+			atEnter={ direction === 'previous' ? rightState : leftState }
+			atLeave={ direction === 'previous' ? leftState : rightState }
+			atActive={ activeState }
+			mapStyles={ mapStyles }
+			pathname={ location.pathname }
+			runOnMount={ false }
+		>
+			{ children }
+		</RouteTransition>
+	);
+};
 
 export default PageTransition;
