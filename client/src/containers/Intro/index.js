67931b382566eb502d218546ccceb5a7d6577103
Spen Taylor <spen_@hotmail.co.uk>
2017-01-01 22:11:03 +1100

    Convert Intro component to SLC

--- client/src/containers/Intro/index.js
+++ client/src/containers/Intro/index.js
@@ -1,7 +1,7 @@
 /**
  * External Dependencies
  */
-import React, { Component } from 'react';
+import React from 'react';
 
 /**
  * Internal Dependencies
@@ -13,24 +13,20 @@ import StyledIntro from './styles/Intro';
 import StyledGreeting from './styles/Greeting';
 import StyledLink from './styles/Link';
 
-export class Intro extends Component {
-	render() {
-		return (
-			<StyledIntro onWheel={ this.props.onWheel }>
-				<SocialIconGroup />
-				<Cell>
-					<StyledGreeting>Oh Hey! 👋</StyledGreeting>
-					<p>
-						I'm <StyledLink to="/about">Spen</StyledLink>,
-						a (mostly Front-End) web developer.<br />
-						Please check out <StyledLink to="/projects">my work</StyledLink> &amp;
-						<StyledLink to="/blog"> my blog</StyledLink> and if there's anything else,
-						feel free to <StyledLink to="/contact">get in touch</StyledLink>!
-					</p>
-				</Cell>
-			</StyledIntro>
-		);
-	}
-}
+export const Intro = ( { onWheel } ) => (
+	<StyledIntro onWheel={ onWheel }>
+		<SocialIconGroup />
+		<Cell>
+			<StyledGreeting>Oh Hey! 👋</StyledGreeting>
+			<p>
+				I'm <StyledLink to="/about">Spen</StyledLink>,
+				a (mostly Front-End) web developer.<br />
+				Please check out <StyledLink to="/projects">my work</StyledLink> &amp;
+				<StyledLink to="/blog"> my blog</StyledLink> and if there's anything else,
+				feel free to <StyledLink to="/contact">get in touch</StyledLink>!
+			</p>
+		</Cell>
+	</StyledIntro>
+);
 
 export default WheelForContent( Intro );
